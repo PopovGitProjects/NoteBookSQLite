40e607768b8e308ff277cf77df1f08a351fa434b
PopovGitProjects <popov.workmail@gmail.com>
2022-06-28 18:01:05 +0500

    add navController

--- app/src/main/java/com/example/notebooksqlite/MainActivity.kt
+++ app/src/main/java/com/example/notebooksqlite/MainActivity.kt
@@ -2,13 +2,21 @@ package com.example.notebooksqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.notebooksqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
