b842c581997367ac11d723e37ec6248023ddb2dd
PopovGitProjects <popov.workmail@gmail.com>
2022-06-30 17:34:31 +0500

    Добавил добавления изображений к заметке

--- app/src/main/java/com/example/notebooksqlite/screens/AddFragment.kt
+++ app/src/main/java/com/example/notebooksqlite/screens/AddFragment.kt
@@ -1,9 +1,15 @@
 package com.example.notebooksqlite.screens
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.notebooksqlite.databinding.FragmentAddBinding
@@ -14,6 +20,8 @@ class AddFragment : Fragment() {
     // This property is only valid between onCreateView and
 // onDestroyView.
     private val binding get() = _binding!!
+
+    private var launcher: ActivityResultLauncher<Intent>? = null
 
     companion object {
         fun newInstance() = AddFragment()
@@ -26,6 +34,14 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentAddBinding.inflate(inflater, container, false)
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                val intentData = Uri.parse(result?.data.toString())
+                binding.imgAdded.setImageURI(intentData)
+                Log.d("My", intentData.toString())
+            }
+        }
         return binding.root
     }
 
@@ -43,7 +59,9 @@ class AddFragment : Fragment() {
             fabAddImage.visibility = View.VISIBLE
         }
         imgBtnEdit.setOnClickListener {
-
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            launcher?.launch(intent)
         }
     }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
